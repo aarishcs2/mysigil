2dcea7d6955830dc1048fbecb3629f4d1680e4fc
Naveed Ahmed <76599103+NaveedAhmed55@users.noreply.github.com>
2023-10-05 20:39:25 +0500

    footer & header added
    the home page footer design and footer design have been added further work in progress

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-6xl text-blue-800 w-[60%] text-center leading-10">
+        Create, Install & Update <br /> Email signatures in one click <br /> across your
+        organization
+      </h1>
+    </main>
+  );
+}
