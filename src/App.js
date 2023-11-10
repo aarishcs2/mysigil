675cb999410a1c74c7fa8b06817202f3993cbf69
Shahbaz Ali <mshahbazali563@gmail.com>
2023-11-10 17:33:55 +0500

    Fixed Issues and Warning

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import AppRoute from "../src/config/routes";
 import "../src/style/style.css";
 import "./App.css";
 import { AuthProvider } from "./context/AuthContext";
-import AppRoute from "../src/config/routes";
-import Home from "./views/Home";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [token, setToken] = useState(false);
-  // useEffect(() => {
-  //   const access_token = localStorage.getItem("access_token");
-  //   setToken(access_token);
-  // }, []);
- 
   return (
     <AuthProvider>
       <AppRoute />
