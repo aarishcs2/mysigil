bfcdb342a77d77315c9b71073710c125d01fbda7
Shubham Kharche <93247322+OnShubham@users.noreply.github.com>
2023-11-13 12:53:10 +0530

    home-screen

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,48 @@
 import React, { useContext } from "react";
 import { Button } from "antd";
 import { AuthContext } from "../context/AuthContext";
-import "../style/style.css";
-import { useNavigate } from "react-router-dom";
+import "../views/Home/style.css";
+import logo from "../views/Home/img/logo.png";
 function Header() {
   const { setShowPopup, setPopupType, token } = useContext(AuthContext);
   const access_token = localStorage.getItem("access_token") ?? token;
-  const navigate = useNavigate();
   const handleButtonClick = (type) => {
     setShowPopup(true);
     setPopupType(type);
   };
   return (
-    <header>
-      <div className="logo">Scribe.</div>
+    <header className="header">
+      {/* logo */}
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+
+      {/* Tab  */}
+      <div className="tab">
+        <ul className="home">
+          <li>
+            <a href="/" onClick={() => ('HOME')}>HOME</a>
+          </li>
+          <li>
+            <a href="/" onClick={() => ('MYSIGIL')}>MYSIGIL</a>
+          </li>
+          <li>
+            <a href="/" onClick={() => ('COMMUNITY')}>COMMUNITY</a>
+          </li>
+          <li>
+            <a href="/" onClick={() => ('PLANS')}>PLANS</a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Button */}
+
+      <div className="Btn-Header">
+        <Button className="btn1" onClick={() => handleButtonClick("login")}>LOG IN </Button>
+        <Button className="btn2" onClick={() => handleButtonClick("register")}>SIGN UP</Button>
+      </div>
+
+
       {!access_token ? (
         <div className="buttons">
           <Button type="link" onClick={() => handleButtonClick("login")}>
@@ -25,11 +54,8 @@ function Header() {
           </Button>
         </div>
       ) : (
-        <div className="buttons">
-          <Button type="primary" onClick={() => navigate("/dashboard")}>
-            Go to workspace
-          </Button>
-        </div>
+        
+        <></>
       )}
     </header>
   );
