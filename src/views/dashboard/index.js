e05b460013346c2abe42606fed405ad469a89644
Michael Nganga <ngangamichael83@gmail.com>
2023-11-13 07:30:21 +0300

    chart and cards designed for the dashboard

--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -4,125 +4,99 @@ import { Icon } from "@iconify/react";
 import Chart from "chart.js/auto";
 import { Line } from "react-chartjs-2";
 
-const labels = ["January", "February", "March", "April", "May", "June"];
+const labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 const data = {
   labels: labels,
   datasets: [
     {
       label: "Impression",
-      backgroundColor: "rgb(255, 99, 132)",
-      borderColor: "rgb(255, 99, 132)",
-      data: [0, 10, 5, 2, 20, 30, 45],
+      backgroundColor: "rgb(15, 15, 97)",
+      borderColor: "red",
+      tension: "0.4",
+      data: [0, 10, 5, 2, 20, 30, 45, 40, 39, 43, 50, 60 ],
     },
     {
         label: "Views",
         backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "rgb(255, 99, 132)",
-        data: [0, 10, 5, 2, 20, 30, 45],
-      },
-      {
+        borderColor: "blue",
+        tension: "0.4",
+        data: [3, 20, 8, 12, 17, 40, 65, 60, 60, 60, 61, 60],
+    },
+    {
         label: "Clicks",
         backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "rgb(255, 99, 132)",
-        data: [0, 10, 5, 2, 20, 30, 45],
+        borderColor: "green",
+        tension: "0.4",  
+        data: [10, 20, 15, 26, 38, 52, 25, 25, 30, 35, 36, 37],
       },
       {
         label: "CTR",
         backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "rgb(255, 99, 132)",
-        data: [0, 10, 5, 2, 20, 30, 45],
+        borderColor: "orange",
+        tension: "0.4",
+        data: [7, 10, 50, 20, 44, 33, 35, 37, 40, 40, 39, 40 ],
       },
   ],
 };
 
 export default function dashboard() {
-    const items = [
-        {
-            key: "1",
-            label: "Chart",
-            children: (
-              <>
-                <h4 className="mt-4 mb-2">Chart</h4>
-                <p className="mb-3">
-                  Chart Immpressions, views and clicks
-                </p>
-
-                <div>
-                  <Line data={data} />
-              </div>
-                
-              </>
-            ),
-        },
-        {
-          key: "2",
-          label: "Co-workers",
-          children: (
-            <>
-              <h4 className="mt-4 mb-2">Co-workers</h4>
-              <p className="mb-3">
-                List of co-workersin there specific departments
-              </p>
-            </>
-          ),
-        },
-        {
-          key: "3",
-          label: "Contacts",
-          children: (
-            <>
-              <h4 className="mt-4 mb-2">Contacts</h4>
-              <p className="mb-3">
-                Contact list of all co-workers
-              </p>
-            </>
-          ),
-        },
-        {
-            key: "4",
-            label: "Departments",
-            children: (
-              <>
-                <h4 className="mt-4 mb-2">Departments</h4>
-                <p className="mb-3">
-                  LIst of created departments 
-                </p>
-              </>
-            ),
-          },
-          {
-            key: "5",
-            label: "Signatures Installed",
-            children: (
-              <>
-                <h4 className="mt-4 mb-2">Signatures Installed</h4>
-                <p className="mb-3">
-                  List of signatures created and installed
-                </p>
-              </>
-            ),
-          },
-          {
-            key: "6",
-            label: "Completed Information",
-            children: (
-              <>
-                <h4 className="mt-4 mb-2">Completed Information</h4>
-                <p className="mb-3">
-                  List of completed information on co-workers
-                </p>
-                
-              </>
-            ),
-          },
-          
-      ];
+    
   return (
     <>
-        <div>
+        {/* <div>
             <h2>Dashboard</h2>
             <Tabs defaultActiveKey="1" items={items} />
+        </div> */}
+        <h2>Dashboard</h2>
+        <div className='row'>
+          {/* card 1  */}
+          <div className='col-md-3'>
+            <div class="card" style={{borderColor: 'red'}}>
+              <div class="card-body">
+                <h5 class="card-title">Impressions</h5>
+                <h6 class="card-subtitle mb-2 text-muted">567</h6>
+                {/* <p class="card-text">Some quick example.</p> */}
+              </div>
+            </div>
+          </div>
+
+          {/* card 2  */}
+          <div className='col-md-3'>
+            <div class="card" style={{borderColor: 'blue'}}>
+              <div class="card-body">
+                <h5 class="card-title">Views</h5>
+                <h6 class="card-subtitle mb-2 text-muted">2192</h6>
+                {/* <p class="card-text">Some quick example.</p> */}
+              </div>
+            </div>
+          </div>
+
+          {/* card 3  */}
+          <div className='col-md-3'>
+            <div class="card" style={{borderColor: 'green'}}>
+              <div class="card-body">
+                <h5 class="card-title">Clicks</h5>
+                <h6 class="card-subtitle mb-2 text-muted">234</h6>
+                {/* <p class="card-text">Some quick example.</p> */}
+              </div>
+            </div>
+          </div>
+
+          {/* card 4  */}
+          <div className='col-md-3'>
+            <div class="card" style={{borderColor: 'orange'}}>
+              <div class="card-body">
+                <h5 class="card-title">CTR</h5>
+                <h6 class="card-subtitle mb-2 text-muted">254</h6>
+                {/* <p class="card-text">Some quick example.</p> */}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div style={{width: "100%", height: "50%"}}>
+            <Line data={data} />
         </div>
     </>
   )
