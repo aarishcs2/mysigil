ffe1ed7dabf38e520bca6f7e42488b727e1991b9
Michael Nganga <ngangamichael83@gmail.com>
2023-11-13 08:24:02 +0300

    cards and chart are designed, yet to feed live data

--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -1,6 +1,7 @@
 import { Tabs } from "antd";
 import React from 'react';
-import { Line } from "react-chartjs-2";
+import { Chart as ChartJS } from 'chart.js/auto'
+import { Chart, Line }            from 'react-chartjs-2'
 
 const labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
@@ -15,26 +16,26 @@ const data = {
       data: [0, 10, 5, 2, 20, 30, 45, 40, 39, 43, 50, 60 ],
     },
     {
-        label: "Views",
-        backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "blue",
-        tension: "0.4",
-        data: [3, 20, 8, 12, 17, 40, 65, 60, 60, 60, 61, 60],
+      label: "Views",
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "blue",
+      tension: "0.4",
+      data: [3, 20, 8, 12, 17, 40, 65, 60, 60, 60, 61, 60],
     },
     {
-        label: "Clicks",
-        backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "green",
-        tension: "0.4",  
-        data: [10, 20, 15, 26, 38, 52, 25, 25, 30, 35, 36, 37],
-      },
-      {
-        label: "CTR",
-        backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "orange",
-        tension: "0.4",
-        data: [7, 10, 50, 20, 44, 33, 35, 37, 40, 40, 39, 40 ],
-      },
+      label: "Clicks",
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "green",
+      tension: "0.4",  
+      data: [10, 20, 15, 26, 38, 52, 25, 25, 30, 35, 36, 37],
+    },
+    {
+      label: "CTR",
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "orange",
+      tension: "0.4",
+      data: [7, 10, 50, 20, 44, 33, 35, 37, 40, 40, 39, 40 ],
+    },
   ],
 };
 
@@ -42,10 +43,6 @@ export default function dashboard() {
     
   return (
     <>
-        {/* <div>
-            <h2>Dashboard</h2>
-            <Tabs defaultActiveKey="1" items={items} />
-        </div> */}
         <h2>Dashboard</h2>
         <div className='row'>
           {/* card 1  */}
@@ -53,7 +50,7 @@ export default function dashboard() {
             <div class="card" style={{borderColor: 'red'}}>
               <div class="card-body">
                 <h5 class="card-title">Impressions</h5>
-                <h6 class="card-subtitle mb-2 text-muted">567</h6>
+                <h6 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-face-smile"></i> 567</h6>
                 {/* <p class="card-text">Some quick example.</p> */}
               </div>
             </div>
@@ -64,7 +61,7 @@ export default function dashboard() {
             <div class="card" style={{borderColor: 'blue'}}>
               <div class="card-body">
                 <h5 class="card-title">Views</h5>
-                <h6 class="card-subtitle mb-2 text-muted">2192</h6>
+                <h6 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-eye"></i> 2192</h6>
                 {/* <p class="card-text">Some quick example.</p> */}
               </div>
             </div>
@@ -75,7 +72,7 @@ export default function dashboard() {
             <div class="card" style={{borderColor: 'green'}}>
               <div class="card-body">
                 <h5 class="card-title">Clicks</h5>
-                <h6 class="card-subtitle mb-2 text-muted">234</h6>
+                <h6 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-computer-mouse"></i> 234</h6>
                 {/* <p class="card-text">Some quick example.</p> */}
               </div>
             </div>
