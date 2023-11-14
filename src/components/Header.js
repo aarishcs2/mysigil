b8da94bc97853cacb9694c4165b2e8cca405d55b
Michael Nganga <ngangamichael83@gmail.com>
2023-11-14 11:17:53 +0300

    Revert "made changes on home screen"
    This reverts commit e911271c32e70277fd7ced43acc609bdb55d380c, reversing
    changes made to 6bcf53d91c2c6c5a048f61d72d3a42c1f0f2368f.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,48 +2,19 @@
 import React, { useContext } from "react";
 import { Button } from "antd";
 import { AuthContext } from "../context/AuthContext";
-import "../views/Home/style.css";
-import logo from "../views/Home/img/logo.png";
+import "../style/style.css";
+import { useNavigate } from "react-router-dom";
 function Header() {
   const { setShowPopup, setPopupType, token } = useContext(AuthContext);
   const access_token = localStorage.getItem("access_token") ?? token;
+  const navigate = useNavigate();
   const handleButtonClick = (type) => {
     setShowPopup(true);
     setPopupType(type);
   };
   return (
-    <header className="header">
-      {/* logo */}
-      <div className="logo">
-        <img src={logo} alt="logo" />
-      </div>
-
-      {/* Tab  */}
-      <div className="tab">
-        <ul className="home">
-          <li>
-            <a href="/" onClick={() => ('HOME')}>HOME</a>
-          </li>
-          <li>
-            <a href="/" onClick={() => ('MYSIGIL')}>MYSIGIL</a>
-          </li>
-          <li>
-            <a href="/" onClick={() => ('COMMUNITY')}>COMMUNITY</a>
-          </li>
-          <li>
-            <a href="/" onClick={() => ('PLANS')}>PLANS</a>
-          </li>
-        </ul>
-      </div>
-
-      {/* Button */}
-
-      <div className="Btn-Header">
-        <Button className="btn1" onClick={() => handleButtonClick("login")}>LOG IN </Button>
-        <Button className="btn2" onClick={() => handleButtonClick("register")}>SIGN UP</Button>
-      </div>
-
-
+    <header>
+      <div className="logo">Scribe.</div>
       {!access_token ? (
         <div className="buttons">
           <Button type="link" onClick={() => handleButtonClick("login")}>
@@ -54,8 +25,11 @@ function Header() {
           </Button>
         </div>
       ) : (
-        
-        <></>
+        <div className="buttons">
+          <Button type="primary" onClick={() => navigate("/dashboard")}>
+            Go to workspace
+          </Button>
+        </div>
       )}
     </header>
   );
