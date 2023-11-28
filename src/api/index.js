3003b6b9e4c9b3354de684949d733d61ea048248
Shahbaz Ali <mshahbazali563@gmail.com>
2023-11-28 11:51:11 +0500

    Changed Base url

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://mysigilbackend.onrender.com",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://mysigilbackend.onrender.com",
+  // baseURL: "http://localhost:5000",
   headers: { "X-Custom-Header": "foobar" },
 });
 
