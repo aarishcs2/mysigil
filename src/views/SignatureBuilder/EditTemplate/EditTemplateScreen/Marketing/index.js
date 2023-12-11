f610819d8e93241b9ec8caee1018c709613e4681
Shahbaz Ali <mshahbazali563@gmail.com>
2023-12-11 21:28:54 +0500

    Worked on Signature Builder Social and Other Screens

---
+++ src/views/SignatureBuilder/EditTemplate/EditTemplateScreen/Marketing/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+function Marketing() {
+  return (
+    <div className="d-flex">
+      <div className="Gallery"></div>
+      <div className="mainScreen"></div>
+    </div>
+  );
+}
+
+export default Marketing;
